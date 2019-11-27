f7cb3e818ab0f9cbd41ce85197941429eed6d9e2
HippywithAnAK <rjrferens@gmail.com>
2019-11-28 12:59:13 +1300

    Added counter functionality

--- game.js
+++ game.js
@@ -11,25 +11,29 @@ function bindEventListeners (dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
-    dots[i].addEventListener('contextmenu', makeGreen)
-    dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('contextmenu', makeGreen);
+    dots[i].addEventListener('click', makeBlue);
+    dots[i].addEventListener('dblclick', hide);
   }
 }
 
 function makeGreen (evt) {
-  evt.preventDefault()
-  evt.target.classList.toggle('green')
-  updateCounts()
+  evt.preventDefault();
+  evt.target.classList.toggle('green');
+  updateCounts();
 }
 
 // CREATE FUNCTION makeBlue HERE
 function makeBlue (evt) {
-  evt.target.classList.toggle('blue')
-  updateCounts()
+  evt.target.classList.toggle('blue');
+  updateCounts();
 }
 
 // CREATE FUNCTION hide HERE
-
+function hide (evt) {
+  evt.target.classList.toggle('invisible');
+  updateCounts();
+}
 function updateCounts () {
   var totals = {
     blue: 0,
@@ -38,13 +42,21 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
-
+  var blue = document.querySelectorAll('.blue').length;
+  var green = document.querySelectorAll('.green').length;
+  var invisible = document.querySelectorAll('.invisible').length;
+  totals['blue'] = blue;
+  totals['green'] = green;
+  totals['invisible'] = invisible;
   // Once you've done the counting, this function will update the display
-  displayTotals(totals)
+  displayTotals(totals);
 }
 
 function displayTotals (totals) {
   for (var key in totals) {
-    document.getElementById(key + '-total').innerHTML = totals[key]
+    document.getElementById(key + '-total').innerHTML = totals[key];
   }
 }
+
+
+
